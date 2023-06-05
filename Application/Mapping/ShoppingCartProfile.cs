db259308d7821ff719cb849bf678d18761dd1485
Wedad <wedad.gamal@gmail.com>
2023-06-05 15:15:31 +0300

    Renaming DetailedShoppingCartDto

--- Application/Mapping/ShoppingCartProfile.cs
+++ Application/Mapping/ShoppingCartProfile.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Dtos.ShoppingCartDto;
 using AutoMapper;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Mapping
 {
@@ -14,7 +9,7 @@ namespace Application.Mapping
         public ShoppingCartProfile()
         {
             CreateMap<ShoppingCart, ShoppingCartDto>();
-            CreateMap<ShoppingCart, DetailedShppingCartDto>();
+            CreateMap<ShoppingCart, DetailedShoppingCartDto>();
         }
     }
 }
