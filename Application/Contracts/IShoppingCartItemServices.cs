784db3eeeb17016db764b9de59f2ae1074994a49
Wedad <wedad.gamal@gmail.com>
2023-06-05 14:52:02 +0300

    Add interfaces

---
+++ Application/Contracts/IShoppingCartItemServices.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Contracts
+{
+    public interface IShoppingCartItemServices
+    {
+        public List<ShoppingCartItem> GetShoppingCartItems();
+        public void AddShoppingCartItem(ShoppingCartItem shoppingCartItem);
+        public void RemoveShoppingCartItem(int shoppingCartItemId);
+        public void RemoveShoppingCartItem(string shoppingCartItemId);
+        public ShoppingCartItem GetShoppingCartItem(int shoppingCartItemId);
+    }
+}
---
+++ Application/Contracts/IShoppingCartServices.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos.ShoppingCartItemDto;
+
+namespace Application.Contracts
+{
+    public interface IShoppingCartServices
+    {
+        public List<ShoppingCartItemDto> GetShoppingCartItems();
+        public void AddShoppingCartItem(ShoppingCartItemDto item);
+        public void RemoveShoppingCartItem(int id);
+        public void DeleteShoppingCartItem(int id);
+        public void EditShoppingCartItem(int id);
+    }
+}
