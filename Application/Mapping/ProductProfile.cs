411ccf91d30efb6cb0ef5d8e45031809da641bda
Mohamed Mahmoud <mohamedhomous27@gmail.com>
2023-06-05 14:49:38 +0300

    Add Mapping Profiles

---
+++ Application/Mapping/ProductProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dtos.ProductDtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        { 
+            CreateMap<Product,MiniProductDto>();
+            CreateMap<Product, DetailedProductDto>();
+            CreateMap<AddProductDto, Product>();
+
+
+        }
+
+    }
+}
---
+++ Application/Mapping/ShoppingCartProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos.ShoppingCartDto;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class ShoppingCartProfile : Profile
+    {
+        public ShoppingCartProfile()
+        {
+            CreateMap<ShoppingCart, ShoppingCartDto>();
+            CreateMap<ShoppingCart, DetailedShppingCartDto>();
+        }
+    }
+}
