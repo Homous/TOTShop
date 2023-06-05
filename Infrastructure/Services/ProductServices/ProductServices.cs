aea47ff5e9bf58b033437080c5181a7cdbc45b05
Mohamed Mahmoud <mohamedhomous27@gmail.com>
2023-06-05 15:13:45 +0300

    Add DependencyInjection For Automapper

--- Infrastructure/Services/ProductServices/ProductServices.cs
+++ Infrastructure/Services/ProductServices/ProductServices.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Contracts.ProuductServices;
 using Application.Dtos.ProductDtos;
+using AutoMapper;
 using Infrastructure.DB;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,12 @@ namespace Infrastructure.Services.ProductServices
     public class ProductServices : IProductServices
     {
         private readonly ApplicationDbContext db;
+        private readonly IMapper mapper;
 
-        public ProductServices(ApplicationDbContext db) 
+        public ProductServices(ApplicationDbContext db,IMapper mapper) 
         {
             this.db = db;
+            this.mapper = mapper;
         }
         public void AddProduct(AddProductDto addProductDto)
         {
@@ -34,12 +37,17 @@ namespace Infrastructure.Services.ProductServices
 
         public List<MiniProductDto> GetProductByName(string name)
         {
-            throw new NotImplementedException();
+            var filter = db.Products.Where(n => n.Name.Contains(name));
+            var getList = filter.ToList();
+            var getWithMap = mapper.Map<List<MiniProductDto>>(getList);
+            return getWithMap;
         }
 
         public List<MiniProductDto> miniDetailsProducts()
         {
-            throw new NotImplementedException();
+            var getMiniPro =  db.Products.ToList();
+            var getWithMap = mapper.Map<List<MiniProductDto>>(getMiniPro);
+            return getWithMap;
         }
 
         public void UpdateProduct(AddProductDto updateProductDto)
