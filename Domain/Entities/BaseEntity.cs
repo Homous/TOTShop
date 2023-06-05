565a21088657d97335ce29e0dd738c62692ace85
Wedad <wedad.gamal@gmail.com>
2023-06-05 13:57:54 +0300

    Test Push

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entities
+﻿namespace Domain.Entities
 {
+    // Base Entity
     public class BaseEntity
     {
         public int Id { get; set; }
