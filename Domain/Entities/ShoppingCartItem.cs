d1d1c6c15dcf2fa28b48f4c088fd9a06adbe943c
Mohamed Mahmoud <mohamedhomous27@gmail.com>
2023-06-05 13:40:05 +0300

    Add project files.

---
+++ Domain/Entities/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ShoppingCartItem : BaseEntity
+    {
+        public decimal TotalCost { get; set; }
+        public int Count { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
