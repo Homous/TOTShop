e2e8fede85697fbf1d10cbba8c24e76b75e8bb2a
Wedad <wedad.gamal@gmail.com>
2023-06-11 16:11:26 +0300

    Add xunit for add shopping cart service

---
+++ Infrastructure.Test/Services/ShoppingCart/ShoppingCartMockData.cs
@@ -0,0 +1,90 @@
+﻿using Application.Dtos.ShoppingCart;
+using Application.Dtos.ShoppingCartItem;
+
+namespace Infrastructure.Test.Services.ShoppingCartTest
+{
+    public class ShoppingCartMockData
+    {
+        public static List<ShoppingCartDto> Get()
+        {
+            return new List<ShoppingCartDto> {
+                new ShoppingCartDto()
+                {
+                    TotalCost = 20,
+                    ShoppingCartItems = new List<ShoppingCartItemDto>()
+                    {
+                        new ShoppingCartItemDto()
+                        {
+                            TotalCost = 5,
+                            Count = 1,
+                            ProductId = 1,
+                            ShoppingCartId =1
+                        },
+                        new ShoppingCartItemDto()
+                        {
+                            TotalCost = 5,
+                            Count = 1,
+                            ProductId = 2,
+                            ShoppingCartId =1
+                        },
+                        new ShoppingCartItemDto()
+                        {
+                            TotalCost = 5,
+                            Count = 1,
+                            ProductId = 3,
+                            ShoppingCartId =1
+                        },
+                        new ShoppingCartItemDto()
+                        {
+                            TotalCost = 5,
+                            Count = 1,
+                            ProductId = 4,
+                            ShoppingCartId =1
+                        }
+                    }
+                }
+            };
+
+        }
+
+        //public ShoppingCart ShoppingCart() { }
+
+        /*  public static List<ShoppingCart> Get() => new List<ShoppingCart> {
+                  new ShoppingCart()
+                  {
+                      TotalCost = 20,
+                      ShoppingCartItems = new List<ShoppingCartItemDto>()
+                      {
+                          new ShoppingCartItem()
+                          {
+                              TotalCost = 5,
+                              Count = 1,
+                              ProductId = 1,
+                              ShoppingCartId =1
+                          },
+                          new ShoppingCartItemDto()
+                          {
+                              TotalCost = 5,
+                              Count = 1,
+                              ProductId = 2,
+                              ShoppingCartId =1
+                          },
+                          new ShoppingCartItemDto()
+                          {
+                              TotalCost = 5,
+                              Count = 1,
+                              ProductId = 3,
+                              ShoppingCartId =1
+                          },
+                          new ShoppingCartItemDto()
+                          {
+                              TotalCost = 5,
+                              Count = 1,
+                              ProductId = 4,
+                              ShoppingCartId =1
+                          }
+                      }
+                  }
+              };*/
+    }
+}
---
+++ Infrastructure.Test/Services/ShoppingCart/TestShoppingCartServices.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Test.Services.ShoppingCart
+{
+    public class TestShoppingCartServices : IDisposable
+    {
+        protected readonly ApplicationDbContext _context;
+        public TestShoppingCartServices()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            _context = new ApplicationDbContext(options);
+
+            _context.Database.EnsureCreated();
+
+        }
+
+        /* [Fact]
+         public async Task GetAllAsync_ShouldReturn200Status()
+         {
+             /// Arrange
+             var shoppingServices = new Mock<IShoppingCartServices>();
+             shoppingServices.Setup(_ => _.GetShoppingCarts()).Returns(shoppingServices.get);
+             var sut = new TodoController(todoService.Object);
+
+             /// Act
+             var result = (OkObjectResult)await sut.GetAllAsync();
+
+
+             // /// Assert
+             result.StatusCode.Should().Be(200);
+         }
+ */
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+    }
+}
