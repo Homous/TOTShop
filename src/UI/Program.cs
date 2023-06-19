0870e45227550823601498cc0f70d1a2b928e69a
Wedad <wedad.gamal@gmail.com>
2023-06-19 16:50:52 +0300

    Change setting of serilog logging to appsetting.json

--- src/UI/Program.cs
+++ src/UI/Program.cs
@@ -5,44 +5,55 @@ using MediatR;
 using Serilog;
 
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .WriteTo.Console(outputTemplate:
-        "[{Timestamp:HH:mm:ss}{Level:u3}] {Message:lj}{NewLine}{Exception}")
-    .WriteTo.File("D:\\ToT\\ToTShop\\src\\Application\\Log\\log.txt", outputTemplate:
-        "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-    .CreateLogger();
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog();
 
-// Add services to the container.
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-//builder.Services.AddControllers();
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
-builder.Services.AddScoped(typeof(IPipelineBehavior<,>),
-    typeof(SerilogBehavior<,>));
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    logger.Information("Starting web application");
+
+    builder.Host.UseSerilog(logger);
+
+    // Add services to the container.
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    //builder.Services.AddControllers();
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+    builder.Services.AddScoped(typeof(IPipelineBehavior<,>),
+        typeof(SerilogBehavior<,>));
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Fatal(ex, "An exception has occured");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
